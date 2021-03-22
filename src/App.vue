9f00b81e9614d12b8971f51d1a051018538a56a7
xzzzz <195286381@qq.com>
2021-03-22 09:44:55 +0800

    更新公司主页

--- src/App.vue
+++ src/App.vue
@@ -3,83 +3,21 @@
     <div class="title-box"></div>
 	<div class="title-line title-cybj">
 		<div class="title-cybj-text">产业布局</div>
+		<Head></Head>
 	</div>
   </div>
 </template>
 
 <script>
+import Head from '@/components/Head'
 export default {
   name: "App",
-  components: {},
+  components: {
+	Head
+  },
 };
 </script>
 
 <style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-  padding: #00f;
-  height: 100%;
-}
-
-#app {
-	position: relative;
-	height: 100%;
-	background-color: gray;
-}
-
-body {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-
-.title-box {
-  height: 84px;
-  background-image: url(./assets/title.png);
-  background-repeat: no-repeat;
-}
-
-.title-cybj {
-	position: absolute;
-	left: 12.3px;
-	top: 129px;
-	width: 394px;
-	height: 4px;
-	margin: 15px;
-	background-image: url(./assets/tltle-line.png);
-	background-repeat: no-repeat;
-	background-color: red;
-}
-
-.title-cybj:link {
-
-}
-
-.title-cybj:visited {
-
-}
-
-.title-cybj:hover {
-
-}
-
-.title-cybj:active {
-
-}
-
-
-.title-cybj-text {
-
-	position: absolute;
-	left: 29px;
-	bottom: 3px;
-
-	line-height: 21px;
-	font-size: 14px;
-	font-family: AlibabaPuHuiTi;
-	font-weight: bold;
-	color: #F7F5F5;
-
-}
 
 </style>
---
+++ src/components/Head.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="demo">
+		Head
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Head',
+	// 返回原始数据.
+	data () {
+		return {
+			name: 'xiaozhui',
+			age: 45
+		}
+	},
+
+	created () {
+		console.log('head 组件创建成功!');
+	}
+}
+</script>
+
+<style>
+
+</style>
